c3712b8466985733432c096bb177652be8d88013
raffaele-99 <raffaele-99@users.noreply.github.com>
2025-06-13 13:48:01 +1000

    brew?

--- hosts/macbook/system/configuration.nix
+++ hosts/macbook/system/configuration.nix
@@ -6,7 +6,6 @@
   
   homebrew = {
     enable = true;
-    
     casks = [
       "spotify"
       "slack"
@@ -15,10 +14,23 @@
       "google-chrome"
       "docker"
       "visual-studio-code"
+      "ghostty"
     ];
+    brews = [
+      "glow"
+    ];
+    onActivation.cleanup = "zap";
+    onActivation.autoUpdate = true;
+    onActivation.upgrade = true;
+    brewPrefix = "/opt/homebrew/bin";
   };
 
-    # macOS user accounts
+  programs.zsh = {
+    enable = true;
+  };
+
+
+
   users.knownUsers = [ "luca" ];
   users.users.luca.uid = 501;
   users.users.luca.home = "/Users/luca";
@@ -26,7 +38,28 @@
   
   system.primaryUser = "luca";
 
-  programs.zsh.enable = true;
+  security.pam.services.sudo_local.touchIdAuth = true;
   
   system.stateVersion = 4;
+
+  # Copy each installed app from /nix/store to /Applications/Nix Apps
+  system.activationScripts.applications.text = let
+    env = pkgs.buildEnv {
+      name = "system-applications";
+      paths = config.environment.systemPackages;
+      pathsToLink = "/Applications";
+    };
+  in
+    pkgs.lib.mkForce ''
+      echo "setting up /Applications..." >&2
+      rm -rf /Applications/Nix\ Apps
+      mkdir -p /Applications/Nix\ Apps
+      find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+      while read -r src; do
+        app_name=$(basename "$src")
+        echo "copying $src" >&2
+        ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+      done
+    '';
+
 }
