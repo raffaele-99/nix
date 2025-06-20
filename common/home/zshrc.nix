b23a1b70af8fa88c048841c3a71fe03d42807300
luca <raffaele-99@users.noreply.github.com>
2025-06-20 17:05:33 +1000

    zsh prompt

--- common/home/zshrc.nix
+++ common/home/zshrc.nix
@@ -3,13 +3,22 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
     
     # Source aliases and functions
     initContent = ''
-      # Source custom aliases and functions
       source ~/.config/zsh/aliases.sh
       source ~/.config/zsh/functions.sh
-      export PS1="%{%F{2}%}%n %{%f%}at %{%F{33}%}%D %* %{%F{2}%}%~ %{%f%}$ "
+
+      git_branch() {
+        git branch 2>/dev/null | sed -n 's/^\* //p'
+      }
+      
+      setopt PROMPT_SUBST
+      export PS1='%{%F{2}%}%n %{%f%}at %{%F{33}%}%D %* %{%F{2}%}%~ %{%F{yellow}%}$(git_branch)%{%f%}$ '
+      
+      ZSH_AUTOSUGGEST_USE_ASYNC=true
     '';
   };
 }
